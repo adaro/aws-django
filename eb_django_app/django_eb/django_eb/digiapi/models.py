f3471c06d57f236a6e8a5ec3410e8eb3c9fd9eda
adaro <alexdaro34@gmail.com>
2016-01-18 21:42:53 -0800

    mongo db setup

--- eb_django_app/django_eb/django_eb/digiapi/models.py
+++ eb_django_app/django_eb/django_eb/digiapi/models.py
@@ -2,19 +2,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from djangotoolbox.fields import ListField
+
+
 class Project(models.Model):
     PROJECT_TYPES = (
         ('I', 'Invitation'),
         ('L', 'Logo'),
-	('D', 'Dayof'),
+        ('D', 'Dayof'),
         ('S', 'Syleshoot'),
         ('A', 'Addressing'),
     )
-    project_id = models.CharField(max_length=250, )
-    project_name = models.CharField(max_length=250)
-    client_name = models.CharField(max_length=250)
+    project_id = models.CharField(max_length=250, null=True)
+    project_name = models.CharField(max_length=250, null=True)
+    client_name = models.CharField(max_length=250, null=True)
     deadline = models.DateTimeField('absolute dealine', default=False, null=True)
     project_type = models.CharField(max_length=1, choices=PROJECT_TYPES)
+    tags = ListField()
 
 
 
