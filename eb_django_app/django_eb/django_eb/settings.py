d6c9738bebd3b42806e3eef997f5b5a02780632b
adaro <alexdaro34@gmail.com>
2016-01-19 21:24:51 -0800

    mongo db

--- eb_django_app/django_eb/django_eb/settings.py
+++ eb_django_app/django_eb/django_eb/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_mongodb_engine',
     'djangotoolbox',
     'permission_backend_nonrel',
     'django_eb.digiapi',
