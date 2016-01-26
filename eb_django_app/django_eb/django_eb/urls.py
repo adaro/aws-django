9651aaa4bcf099ac3852a25bfb43b447f85f5263
adaro <alexdaro34@gmail.com>
2016-01-25 20:54:32 -0800

    mongo db

--- eb_django_app/django_eb/django_eb/urls.py
+++ eb_django_app/django_eb/django_eb/urls.py
@@ -20,5 +20,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', set_project_name),
+    url(r'^views/todo.html$', index),
     url(r'^$', index),
+    # url(r'^views/about.html$', index),
 ]
